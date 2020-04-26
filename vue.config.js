815f4e3f389405be1d78972db73db7dd4fbf8044
Freddie Nelson <freddie0208@hotmail.com>
2020-04-26 14:14:21 +0100

    fixed icons

--- vue.config.js
+++ vue.config.js
@@ -1,46 +1,17 @@
 module.exports = {
   pwa: {
-    icons: [
-      {
-        src: "public/img/icons/icon-72x72.png",
-        sizes: "72x72",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-96x96.png",
-        sizes: "96x96",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-128x128.png",
-        sizes: "128x128",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-144x144.png",
-        sizes: "144x144",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-152x152.png",
-        sizes: "152x152",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-384x384.png",
-        sizes: "384x384",
-        type: "image/png"
-      },
-      {
-        src: "public/img/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }
-    ]
+    name: "TicTacToe",
+    themeColor: "#f5eded",
+    msTileColor: "#f5eded",
+    manifestOptions: {
+      background_color: "#f5eded"
+    },
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/icon-152x152.png",
+      maskIcon: "img/icons/logo.svg",
+      msTileImage: "img/icons/icon-144x144.png"
+    }
   }
 };
